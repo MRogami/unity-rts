ced6afb1c33d381a17f9805245288c04c67c5d72
JonasKoenig <jonas-koenig@gmx.net>
2018-01-06 19:21:08 +0100

    Small navigatable Player and Obstacles
    Small navigatable Player and Obstacles. Navigation via NavMesh

---
+++ Mobile RTS Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+	public LayerMask movementMask;
+
+	Camera cam;
+	PlayerMotor motor;
+
+	void Start ()
+	{
+		cam = Camera.main;
+		motor = GetComponent<PlayerMotor> ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit, 100, movementMask)) {
+				// move player there
+				motor.moveToPoint(hit.point);
+				// stop focus
+			}
+		} else if (Input.GetMouseButtonDown (1)) {
+			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit, 100)) {
+				// check if interactable object
+			}
+		}
+	}
+}
---
+++ Mobile RTS Project/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+	public void moveToPoint (Vector3 point) {
+		agent.SetDestination (point);	
+	}
+}
